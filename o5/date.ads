ade30e59b37414f5b9dde43a9708848ef5912bf7
billyn0treally <billynotreally@protonmail.com>
2023-08-22 23:47:23 +0200

    fuck me

--- o5/date.ads
+++ o5/date.ads
@@ -1,23 +1,19 @@
-with Ada.Text_IO;         use Ada.Text_IO;
-with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
-with Ada.Exceptions;      use Ada.Exceptions;
-with Ada.Float_Text_IO;   use Ada.Float_Text_IO;
-
 package date is
 
     type Date_Type is private;
     type Date_Arr_Type is private;
-    procedure Get(D : in out Date_Type);
-    procedure Put(D : in out Date_Type);
+
+    procedure Get(date : in out Date_Type);
+    procedure Put(date : in out Date_Type);
+
 --    function Next_Date(date : in Date_Type) return Date_Type;
 --    function Previous_Date(date : in Date_Type) return Date_Type;
+
 --    function "<"(Left, Right : in Date_Type) return Boolean;
 --    function ">"(Left, Right : in Date_Type) return Boolean;
 --    function "="(Left, Right : in Date_Type) return Boolean;
 
     Format_Error, Year_Error, Month_Error, Day_Error : exception;
-
-
 
     private
     S : String;
