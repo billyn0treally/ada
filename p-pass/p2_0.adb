d8d9482730a69a3c5cc9f5abe76426335476ad59
billyn0treally <billynotreally@protonmail.com>
2023-09-26 07:57:51 +0200

    p-pass

---
+++ p-pass/p2_0.adb
@@ -0,0 +1,10 @@
+with Ada.Text_IO;                       use Ada.Text_IO;
+with Ada.Integer_Text_IO;               use Ada.Integer_Text_IO;
+with Ada.Float_Text_IO;                 use Ada.Float_Text_IO;
+with Ada.Numerics.Elementary_Functions; use Ada.Numerics.Elementary_Functions;
+
+procedure p2_0 is
+
+begin
+
+end p2_0;
