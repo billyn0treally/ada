030ad2663d6b4ad57a0cbb05293c5dfd05a7e4f7
billyn0treally <billynotreally@protonmail.com>
2023-09-27 11:37:09 +0200

    put deez nuts

--- o3_1/o3_1.adb
+++ o3_1/o3_1.adb
@@ -2,38 +2,7 @@
 with Ada.Text_IO;         use Ada.Text_IO;
 with Ada.Float_Text_IO;   use Ada.Float_Text_IO;
 
---Krav för uppgiften:
-
---CHECK  Felaktigt definierad datatyp
-
---Viktigt för uppgiften:
- 
---CHECK?  Ordning av kodens olika delar
-
---CHECK  Parametrar i underprogram har felaktig mod
-
 --  Skapa en Get/Put för varje datatyp, även inre
-
---CHECK  Saknar specificerare för Put för heltal eller flyttal
-
---CHECK  Onaturligt eller felaktigt formulerade if-satser
-
---CHECK  Rensar inte bufferten innan påföljande inmatning skall påbörjas
-
---Viktigt för framtiden:
-
---CHECK  Undvik att lägga New_line i Put
-
---CHECK?  Kvarvarande tecken i inmatningsbuffert
-
---Tips:
-
---  Använd tecken istället för sträng om möjligt
-
---CHECK Lämna inte in onödiga saker i koden, paket
-
---CHECK?  Använd Put_Line istället
-
 procedure o3_1 is
 
     -- Deklarera posterna
@@ -85,35 +54,52 @@ procedure o3_1 is
         Get(DS1.P);
     end Get;
 
+    -- Get för Post_Type2_N
+    procedure Get(PT2 : out Post_Type2_N) is
+        Ch : Character;
+    begin
+        Get(PT2.S);
+        Get(Ch);
+        Get(PT2.Z);
+    end Get;
+
     -- Get för  Post_Type_2
     procedure Get(DS2: out Post_Type2) is
         Ch : Character;
     begin
-        Get(DS2.D.S);
+        Get(DS2.D);
         Get(Ch);
-        Get(DS2.D.Z);
+        Get(DS2.U);
+    end Get;
+
+    -- Get för Post_Type3_N_1
+    procedure Get(DS3 : out Post_Type3_N_1) is
+        Ch : Character;
+    begin
+        Get(DS3.Y);
         Get(Ch);
-        Get(DS2.U.S);
+        Get(DS3.Q);
+    end Get;
+
+    -- Get för Post_Type3_N_2
+    procedure Get(DS3 : out Post_Type3_N_2) is
+        Ch : Character;
+    begin
         Get(Ch);
-        Get(DS2.U.Z);
+        DS3.T := Ch = 'T';
+        Get(Ch);
+        Get(DS3.L);
     end Get;
 
     -- Get för  Post_Type_3
     procedure Get(DS3: out Post_Type3) is
         Ch : Character;
     begin
-        Get(DS3.J.Y);
+        Get(DS3.J);
         Get(Ch);
-        Get(DS3.J.Q);
+        Get(DS3.B);
         Get(Ch);
-        Get(DS3.B.Y);
-        Get(Ch);
-        Get(DS3.B.Q);
-        Get(Ch);
-        Get(Ch);
-        DS3.O.T := Ch = 'T';
-        Get(Ch);
-        Get(DS3.O.L);
+        Get(DS3.O);
     end Get;
 
     -- Put för Post_Type_1
@@ -124,35 +110,49 @@ procedure o3_1 is
         Put(DS1.P);
     end Put;
 
+    -- Put för Post_Type2_N
+    procedure Put(DS2 : in Post_Type2_N) is
+    begin
+        Put(DS2.S,Fore => 0, Aft =>3, Exp => 0);
+        whitespace;
+        Put(DS2.Z);
+    end Put;
+
     -- Put för Post_Type_2
     procedure Put (DS2 : in Post_Type2) is
     begin
-        Put(DS2.D.S,Fore => 0, Aft =>3, Exp => 0);
+        Put(DS2.D);
         whitespace;
-        Put(DS2.D.Z);
+        Put(DS2.U);
+    end Put;
+
+    -- Put för Post_Type3_N_1
+    procedure Put(DS3 : in Post_Type3_N_1) is
+    begin
+        Put(DS3.Y);
         whitespace;
-        Put(DS2.U.S,Fore => 0,Aft => 3, Exp => 0);
-        whitespace;
-        Put(DS2.U.Z);
+        Put(DS3.Q);
+    end Put;
+
+    -- Put för Post_Type3_N_2
+    procedure Put(DS3 : in Post_Type3_N_2) is
+    begin
+        if DS3.T then
+            Put("True ");
+        else 
+            Put("False ");
+        end if;
+        Put(DS3.L);
     end Put;
 
     -- Put för Post_Type_3
     procedure Put (DS3 : in Post_Type3) is
     begin
-        Put(DS3.J.Y);
+        Put(DS3.J);
         whitespace;
-        Put(DS3.J.Q);
+        Put(DS3.B);
         whitespace;
-        Put(DS3.B.Y);
-        whitespace;
-        Put(DS3.B.Q);
-        whitespace;
-        if DS3.O.T then
-            Put("True ");
-        else 
-            Put("False ");
-        end if;
-        Put(DS3.O.L);
+        Put(DS3.O);
     end Put;
 
 begin
