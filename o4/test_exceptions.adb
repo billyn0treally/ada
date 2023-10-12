0905bb896905df928d82bfde4d67e2a40efdc264
billyn0treally <billynotreally@protonmail.com>
2023-10-12 14:06:39 +0200

    lil ix

--- o4/test_exceptions.adb
+++ o4/test_exceptions.adb
@@ -1,4 +1,4 @@
---mikna021: Samarbetat med melg374, Melker Gustafsson, samma program
+--mikna021: Samarbetat med melgu374, Melker Gustafsson, samma program
 with Ada.Text_IO;         use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Exceptions;      use Ada.Exceptions;
