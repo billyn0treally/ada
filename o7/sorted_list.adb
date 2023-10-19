a5e3f1d969ab92288723fa8d6b9126c404eb3a77
billyn0treally <billynotreally@protonmail.com>
2023-10-19 13:33:47 +0200

    CBT initialization

---
+++ o7/sorted_list.adb
@@ -0,0 +1,10 @@
+--mikna021: Samarbetat med melgu374, Melker Gustafsson, samma program
+with Ada.Text_IO;         use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Exceptions;      use Ada.Exceptions;
+with Ada.Float_Text_IO;   use Ada.Float_Text_IO;
+
+package body sorted_list is
+
+
+end sorted_list;
---
+++ o7/sorted_list.ads
@@ -0,0 +1,29 @@
+--mikna021: Samarbetat med melgu374, Melker Gustafsson, samma program
+package sorted_list is
+
+    No_Such_Element_Error : exception;
+
+    type List_Type is private;
+
+    function Empty(LT : in List_Type) return Boolean;
+    function Member(LT : in List_Type; Key : in Integer) return Boolean;
+    function Length(LT : in List_Type) return Integer;
+
+    procedure Insert(LT : in out List_Type; I : in Integer);
+    procedure Put(LT : in List_Type);
+    procedure Remove(LT : in out List_Type; Key : in Integer);
+    procedure Delete(LT : in out List_Type);
+
+    private 
+
+    type Ptr;
+
+    type List_Type is
+        access Ptr;
+
+        type Ptr is record
+            Data : Integer;
+            Next : List_Type;
+        end record;
+
+end sorted_list;
