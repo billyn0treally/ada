a7ca543f038c44cfe667688f80c0e9b2d7e1935d
billyn0treally <billynotreally@protonmail.com>
2023-11-30 15:50:08 +0100

    fix

--- o5/date.ads
+++ o5/date.ads
@@ -4,15 +4,16 @@ package date is
     Format_Error, Year_Error, Month_Error, Day_Error : exception;
 
     type Date_Type is private;
+
     procedure Get(Item : out Date_Type);
     procedure Put(Item : in Date_Type);
 
     function Next_Date(D : in Date_Type) return Date_Type;
     function Previous_Date(D : in Date_Type) return Date_Type;
 
-    function "<"(L, R: in Date_Type) return Boolean;
-    function ">"(L, R: in Date_Type) return Boolean;
     function "="(L, R: in Date_Type) return Boolean;
+    function ">"(L, R: in Date_Type) return Boolean;
+    function "<"(L, R: in Date_Type) return Boolean;
 
 
     private
