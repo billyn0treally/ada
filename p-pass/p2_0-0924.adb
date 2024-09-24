3f37007862d4bdae7be2d6413634e5c1b10c1896
billyn0treally <billyn0treally@protonmail.com>
2024-09-24 07:41:08 +0200

    update

---
+++ p-pass/p2_0-0924.adb
@@ -0,0 +1,9 @@
+with Ada.Text_IO;                       use Ada.Text_IO;
+with Ada.Integer_Text_IO;               use Ada.Integer_Text_IO;
+with Ada.Float_Text_IO;                 use Ada.Float_Text_IO;
+
+procedure p2_0 is
+
+begin
+
+end p2_0;
