d23440df60b7ccde37909f7f361ae57110a85120
billyn0treally <billynotreally@protonmail.com>
2024-10-24 08:51:31 +0200

    update

--- p-pass/2024/p20-2210.adb
+++ p-pass/2024/p20-2210.adb
@@ -9,6 +9,21 @@ procedure p20 is
         return (Character'Pos(C) - Character'Pos('0')) * Integer'Value(S);
     end "*";
 
+    procedure mul (S : out String ; F : out Float ; N : out Integer) is
+    begin
+    Put("Mata in en sträng med exakt 3 tecken: ");
+    Get(S);
+    Put("Mata in ett flyttal: ");
+    Get(F);
+    N := Integer(Float'Floor((Float'Value(S)) * F));
+    end mul;
+
+    function rad (N : in Integer) return Integer is
+    begin
+        return Integer(Float(N * N * 3.14));
+    end rad;
+
+
     S : String(1..3);
     N: Integer;
     F : Float;
@@ -26,5 +41,24 @@ begin
     Put(C);
     Put(" blir ");
     Put((S*C), Width => 1);
+    New_Line(2);
+    Skip_Line;
+    
+    mul(S,F,N);
+    Put("Du matade in strängen: ");
+    Put(S);
+    Put(" och flyttalet: ");
+    Put((F), Fore => 0, Aft => 1, Exp => 0);
+    Put(" och produkten blev ");
+    Put(N);
+    New_Line(2);
+    Skip_Line;
+    
+
+    Put("Mata in längden: ");
+    Get(N);
+    Put("Den beräknade arean blev: ");
+    Put(rad(N), Width => 1);
+    Skip_Line;
 
 end p20;
