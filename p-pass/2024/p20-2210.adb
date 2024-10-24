511564456f76d6dc2bcc15869ed74696f315af60
billyn0treally <billynotreally@protonmail.com>
2024-10-24 07:26:55 +0200

    update

---
+++ p-pass/2024/p20-2210.adb
@@ -0,0 +1,13 @@
+with Ada.Text_IO;                       use Ada.Text_IO;
+with Ada.Integer_Text_IO;               use Ada.Integer_Text_IO;
+with Ada.Float_Text_IO;                 use Ada.Float_Text_IO;
+
+procedure p20 is
+
+
+begin
+
+
+    Put("Mata in ett flyttal: ");
+
+end p20;
