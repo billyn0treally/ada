8593212989aee1a8f8e3808d8fc64a2590b01cf1
billyn0treally <billynotreally@protonmail.com>
2024-10-24 08:18:44 +0200

    update

--- p-pass/2024/p20-2210.adb
+++ p-pass/2024/p20-2210.adb
@@ -4,10 +4,27 @@ with Ada.Float_Text_IO;                 use Ada.Float_Text_IO;
 
 procedure p20 is
 
+    function "*" (S : in String ; C : in Character) return Integer is
+    begin
+        return (Character'Pos(C) - Character'Pos('0')) * Integer'Value(S);
+    end "*";
+
+    S : String(1..3);
+    N: Integer;
+    F : Float;
+    C : Character;
 
 begin
 
-
-    Put("Mata in ett flyttal: ");
+    Put("Mata in en sträng: ");
+    Get(S);
+    Put("Mata in ett tecken: ");
+    Get(C);
+    Put("Den beräknade produkten av ");
+    Put(S);
+    Put(" och ");
+    Put(C);
+    Put(" blir ");
+    Put((S*C), Width => 1);
 
 end p20;
